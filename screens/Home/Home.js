0d7eb3b7ab18f5b8e362b53f042e22793aa06bf5
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-06 11:47:07 -0300

    añade pantalla  Home

---
+++ screens/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet,Text, View} from "react-native"
+const Home = ({investigation}) =>{
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+export default Home;
+
+const style = StyleSheet.create({
+    container:{
+        flex:1
+    }
+})
