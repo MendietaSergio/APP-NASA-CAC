20a24055e25e6e4af5d73db7958258ea80dea07b
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-07 10:48:45 -0300

    añade boton para agregar un robot

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import { StyleSheet,Text, View} from "react-native"
-const Home = ({investigation}) =>{
-    return (
-        <View style={styles.container}>
-
-        </View>
-    )
-}
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+const Home = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity 
+            activeOpacity={0.7} 
+            style={styles.btn} 
+            onPress={() => navigation.navigate('Rover')}>
+        <AntDesign name="plus" size={24} color="#FFF" />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default Home;
 
-const style = StyleSheet.create({
-    container:{
-        flex:1
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  btn: {
+    display: "flex",
+    position: "absolute",
+    backgroundColor: "#142950",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    bottom: 30,
+    right: 30,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    padding: 5,
+  },
+});
