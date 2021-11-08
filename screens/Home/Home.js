0dec133c8bb95fd0edd2be78516394a6a4928b72
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-11-08 15:55:56 -0300

    añade cambios en Home

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,9 +1,42 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, {useState, useEffect} from "react";
+import { StyleSheet, View, TouchableOpacity, FlatList } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
+import Item from './components/Item';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useIsFocused} from '@react-navigation/native';
+
+
 const Home = ({ navigation }) => {
+  const isFocused = useIsFocused();
+  const [rovers, setRovers] = useState([]);
+
+  useEffect(async () => {
+    if (isFocused) {
+      const value = await AsyncStorage.getItem('rovers');
+      if (value) setRovers(JSON.parse(value));
+    }
+  }, [isFocused]);
+
+  const onDelete = async item => {
+    try {
+      const current = rovers.filter(value => value.cod !== item.cod);
+      const json_value = JSON.stringify(current);
+      await AsyncStorage.setItem('rovers', json_value);
+      setRovers(current);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <FlatList
+        data={rovers}
+        renderItem={props => (
+          <Item {...props} navigation={navigation} onDelete={onDelete} />
+        )}
+        keyExtractor={item => item.name}
+      />
       <TouchableOpacity 
             activeOpacity={0.7} 
             style={styles.btn} 
